3d11132e0ae2b26677c39bc1099b6df894a2fd91
pinqy <19714467+pinqy@users.noreply.github.com>
2024-08-30 02:24:02 -0400

    Add routing for page content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
