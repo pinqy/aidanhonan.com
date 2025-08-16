e712607ca75b284f631e651f5ace77252c4181b7
pinqy <19714467+pinqy@users.noreply.github.com>
2025-08-15 22:15:09 -0400

    Add games page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { BackendTestComponent } from './pages/backend-test/backend-test.component';
+import { GamesComponent } from './pages/games/games.component';
 
 export const routes: Routes = [
     { path: '', title: 'Aidan Honan', component: HomePageComponent },
     { path: 'backend-test', title: 'Backend Test | Aidan Honan', component: BackendTestComponent },
+    { path: 'games', title: 'Games | Aidan Honan', component: GamesComponent },
     { path: '**', title: 'Page Not Found | Aidan Honan', component: PageNotFoundComponent },
 ];
---
+++ src/app/pages/games/games.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GamesComponent } from './games.component';
+import { appConfig } from '../../app.config';
+
+describe('GamesComponent', () => {
+  let component: GamesComponent;
+  let fixture: ComponentFixture<GamesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GamesComponent],
+      providers: appConfig.providers,
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GamesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
