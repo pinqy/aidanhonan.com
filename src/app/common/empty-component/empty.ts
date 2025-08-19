d7b93cb05a492e5b581770f8982b88d4fafe8960
pinqy <19714467+pinqy@users.noreply.github.com>
2025-08-18 23:44:40 -0400

    Remove separate GamesMenu component

---
+++ src/app/common/empty-component/empty.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty',
+  imports: [],
+  standalone: true,
+  template: ``,
+  styles: ``
+})
+export class EmptyComponent {}
--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Game, GAMES } from './games-constants';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './games.component.html',
   styleUrl: './games.component.scss'
 })
 export class GamesComponent {
+  games: Game[] = []
+  gameIconPathDefault = "game-icon-default.png"
+
+  constructor() {
+    this.games = GAMES
+  }
 }
