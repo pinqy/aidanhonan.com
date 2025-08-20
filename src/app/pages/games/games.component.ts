d7c77d014c9afc0529c4cf7846e065007c21ab4c
pinqy <19714467+pinqy@users.noreply.github.com>
2025-08-19 22:44:41 -0400

    Add games detail focus

--- src/app/pages/games/games.component.ts
+++ src/app/pages/games/games.component.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Event, NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import { Game, GAMES } from './games-constants';
 import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './games.component.html',
   styleUrl: './games.component.scss'
 })
 export class GamesComponent {
+  private readonly router = inject(Router)
+  private readonly gameDetailRegex: RegExp = /\/games\/\S+/
+  isGameDetailView = false
+
   games: Game[] = []
   gameIconPathDefault = "game-icon-default.png"
 
   constructor() {
+    // When navigating off of "/games" page, disable games menu links
+    // and display the games detail page
+    this.router.events.pipe(takeUntilDestroyed()).subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.gameDetailRegex.test(event.url)) {
+          this.isGameDetailView = true
+        } else {
+          this.isGameDetailView = false
+        }
+      }
+    })
+
     this.games = GAMES
   }
 }
