e7a5e4c0fecf94e6bf3020326bff89986cb5fe0c
pinqy <19714467+pinqy@users.noreply.github.com>
2026-01-04 03:19:59 -0500

    Add basic sizing service + make home page more responsive

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { SizeService } from '../../../services/size-service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.scss'
 })
 export class HomePageComponent {
-
+  sizeService = inject(SizeService);
+  bannerTextSize = computed(() => {
+    if (this.sizeService.isXSmall()) {return "2rem"}
+    else if (this.sizeService.isSmall()) {return "2.25rem"}
+    else if (this.sizeService.isMedium()) {return "2.75rem"}
+    else if (this.sizeService.isLarge()) {return "3.5rem"}
+    else if (this.sizeService.isXLarge()) {return "4rem"}
+    else {return "2.5rem"}
+  })
 }
---
+++ src/services/size-service.ts
@@ -0,0 +1,31 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout";
+import { computed, inject, Injectable, Signal, signal, WritableSignal } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class SizeService {
+    // Reference: https://material.angular.dev/cdk/layout/overview
+    private breakpointObserver = inject(BreakpointObserver)
+    private readonly trackedBreakpoints = [Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge]
+
+    private _currentBreakpoint: WritableSignal<string> = signal('')
+
+    // ReadOnly signals for components to use
+    readonly currentBreakpoint: Signal<string> = this._currentBreakpoint.asReadonly()
+    readonly isXSmall: Signal<boolean> = computed(() => this._currentBreakpoint() == Breakpoints.XSmall)
+    readonly isSmall: Signal<boolean> = computed(() => this._currentBreakpoint() == Breakpoints.Small)
+    readonly isMedium: Signal<boolean> = computed(() => this._currentBreakpoint() == Breakpoints.Medium)
+    readonly isLarge: Signal<boolean> = computed(() => this._currentBreakpoint() == Breakpoints.Large)
+    readonly isXLarge: Signal<boolean> = computed(() => this._currentBreakpoint() == Breakpoints.XLarge)
+
+    constructor() {
+        this.breakpointObserver.observe(this.trackedBreakpoints).subscribe((state) => this.updateCurrentBreakpoint(state))
+    }
+
+    private updateCurrentBreakpoint(state: BreakpointState): void {
+        // find first active breakpoint (the ones I'm using should be non-overlapping based on the docs)
+        const firstActiveBreakpoint = this.trackedBreakpoints.find((tb) => state.breakpoints[tb])
+        if (firstActiveBreakpoint) {
+            this._currentBreakpoint.set(firstActiveBreakpoint)
+        }
+    }
+}
